f2646257d14f0a31221c5eaf738c9c8f7acc8d9a
kycarr <whizbon@gmail.com>
2018-02-11 13:43:24 -0800

    adding simple vive controls

---
+++ VRPot/Assets/Scripts/Clay/ClayMesh.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class ClayMesh : MonoBehaviour {
+
+    public float radius = 1f;
+    public float height = 1f;
+    public float thickness = 0.2f;
+    public int numRings = 2;
+    public int numRingPoints = 10;
+
+    private Vector3[] vertices;
+    private Mesh mesh;
+
+    // Generate the clay mesh
+    public void Generate()
+    {
+    }
+}
